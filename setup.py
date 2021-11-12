b39e094d39b74dbb6689accd7adb2eb3b1d87959
Harsha <harshakokel@gmail.com>
2021-11-12 09:50:09 -0600

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='crafting world',
+    version='',
+    packages=['multiworld', 'multiworld.core', 'multiworld.envs', 'multiworld.envs.mujoco',
+              'multiworld.envs.mujoco.util', 'multiworld.envs.mujoco.sawyer_xyz',
+              'multiworld.envs.mujoco.sawyer_torque', 'multiworld.envs.pygame', 'multiworld.envs.gridworlds',
+              'multiworld.envs.real_world', 'multiworld.envs.real_world.sawyer', 'crafting_env'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
